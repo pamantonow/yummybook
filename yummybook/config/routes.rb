bc26ba6167743c663b31295c7c1dd1fec4d6baaf
pamantonow <unstyleurbain@gmail.com>
2016-02-23 18:48:45 -0600

    Add it simple profile

--- yummybook/config/routes.rb
+++ yummybook/config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   # Example of regular route:
    # get 'products/:id' => 'catalog#view'
    get 'login' => "sessions#new", as: "users_login"
-   post 'login' => "sessions#login", as: "login"
+   post 'login' => "sessions#create", as: "login"
+   get 'profile' => 'users#show', as: "profile"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
