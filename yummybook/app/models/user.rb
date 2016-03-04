be5353f75041ed7a936f1417b7e36e292dd274bf
pamantonow <unstyleurbain@gmail.com>
2016-03-03 18:13:50 -0700

    Add it email validation and fixed the bug in the profile

--- yummybook/app/models/user.rb
+++ yummybook/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :recipes
+  has_many :saved_categories, through: :recipes, source: :category
   validates :username, presence: true
-  validates :email ,presence: true , uniqueness: true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, presence: true , uniqueness: true
 
   has_secure_password
 end
