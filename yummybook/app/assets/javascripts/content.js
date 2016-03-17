9fcb0a193c13c1d6b4d684d0c3e8cf0a1bed71f7
pamantonow <unstyleurbain@gmail.com>
2016-03-16 19:27:22 -0600

    need to fix

--- yummybook/app/assets/javascripts/content.js
+++ yummybook/app/assets/javascripts/content.js
@@ -1,1 +1,11 @@
-alert("Hello from your Chrome extension!")
+chrome.runtime.onMessage.addListener(
+  function(request,sender,sendResponse){
+    if (request.message == "clicked_browser_action"){
+        var firstHref = $("a[href^='http']").eq(0).attr("href");
+        console.log(firstHref);
+
+        chrome.runtime.sendMessage({"message": "open new tab", "url": firstHref});
+        alert("Does it still works?")
+    }
+  }
+);
