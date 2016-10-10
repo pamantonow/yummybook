1c6e072ce9b41cf2b0f4b60a4db08465d6f20c81
pamantonow <unstyleurbain@gmail.com>
2016-10-10 15:59:46 -0600

    need to finish

--- yummybook/app/models/user.rb
+++ yummybook/app/models/user.rb
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, presence: true , uniqueness: true
 
   has_secure_password
+
+  mapping do 
+  	indexes :recipes
+  end
+
+  def as_indexed_json
+ 	 self.as_json({
+    only: [:username],
+    include: {
+      author: { :id, :name, :description, :ingredients, :instructions},
+  	})
+	end
+
 end
